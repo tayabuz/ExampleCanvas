bb9b748c367b8032ac0e8f93676033f710fa27ee
tayabuz <taya.buzhinskaya@gmail.com>
2020-08-10 17:00:11 +0500

    Fix uncorrect results in getTextHeight.

--- app/src/main/java/com/example/examplecanvas/Artifacts/CenteredStringArtifact.kt
+++ app/src/main/java/com/example/examplecanvas/Artifacts/CenteredStringArtifact.kt
@@ -12,13 +12,10 @@ class CenteredStringArtifact(context: Context, rect: Rect, text: String, typefac
     override fun drawText()
     {
         setTextSizeForWidth(textPaint, rect, text)
+        val bounds =  calculateBounds(textPaint, rect, text)
 
-        val bounds = RectF(rect)
-        bounds.right = textPaint.measureText(text, 0, text.length)
-        bounds.bottom = textPaint.descent() - textPaint.ascent()
-
-        bounds.left += (rect.width() - bounds.right) / 2f
-        bounds.top += (rect.height() - bounds.bottom) / 2f
+        extraCanvas.drawLine(rect.left.toFloat(), rect.exactCenterY(), rect.right.toFloat(), rect.exactCenterY(), paint) //for testing
+        extraCanvas.drawLine(rect.exactCenterX(), rect.top.toFloat(), rect.exactCenterX(), rect.bottom.toFloat(), paint) //for testing
 
         extraCanvas.drawText(text, bounds.left, bounds.top - textPaint.ascent(), textPaint)
         extraCanvas.drawRect(rect, paint) //for testing
--- app/src/main/java/com/example/examplecanvas/Artifacts/FittedInRectStringArtifact.kt
+++ app/src/main/java/com/example/examplecanvas/Artifacts/FittedInRectStringArtifact.kt
@@ -1,11 +1,11 @@
 package com.example.examplecanvas.Artifacts
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
-import android.graphics.Typeface
+import android.graphics.*
 import android.text.TextPaint
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.roundToInt
 
 open class FittedInRectStringArtifact(context: Context, val rect: Rect, val text: String, val typefaceFont: Typeface, val colorForText: Int) : DrawArtifact(context) {
     protected var textPaint: TextPaint = TextPaint()
@@ -29,24 +29,37 @@ open class FittedInRectStringArtifact(context: Context, val rect: Rect, val text
         var desiredTextSize = testTextSize * rect.width() / paint.measureText(text)
         paint.textSize = desiredTextSize
 
-        while (getTextHeight(textPaint, text) > rect.height())
+        while (getTextHeight(textPaint) > rect.height())
         {
             desiredTextSize -= 0.1f
             paint.textSize = desiredTextSize
         }
     }
 
-    protected fun getTextHeight(paint: Paint, text: String): Int {
-        val bounds = Rect()
-        paint.getTextBounds(text, 0, text.length, bounds)
-        return bounds.height()
+    protected fun getTextHeight(paint: Paint): Int {
+        return ceil(paint.fontMetrics.descent - paint.fontMetrics.ascent).toInt()
+    }
+
+    protected fun calculateBounds(paint: Paint, rect: Rect, text: String) : RectF {
+        val bounds = RectF(rect)
+        bounds.right = paint.measureText(text, 0, text.length)
+        bounds.bottom = paint.descent() - paint.ascent()
+
+        bounds.left += (rect.width() - bounds.right) / 2f
+        bounds.top += (rect.height() - bounds.bottom) / 2f
+        return bounds
     }
 
     protected open fun drawText()
     {
         setTextSizeForWidth(textPaint, rect, text)
-        extraCanvas.drawText(text, rect.left.toFloat(),  rect.exactCenterY() + getTextHeight(textPaint, text)/2f, textPaint)
+        val bounds = calculateBounds(textPaint, rect, text)
+
+        extraCanvas.drawText(text, rect.left.toFloat(),   bounds.top - textPaint.ascent(), textPaint)
+
+        extraCanvas.drawLine(rect.left.toFloat(), rect.exactCenterY(), rect.right.toFloat(), rect.exactCenterY(), paint) //for testing
         extraCanvas.drawRect(rect, paint) //for testing
+
         extraCanvas.save()
         extraCanvas.restore()
     }
